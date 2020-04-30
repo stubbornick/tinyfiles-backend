7e8bad4774eb1925d9fadacb44b85c25c466c17f
stubbornick <38818331+stubbornick@users.noreply.github.com>
2020-04-30 18:24:08 +0200

    Lint configs

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -3,14 +3,13 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
   extends: [
     'airbnb-typescript/base',
     'plugin:import/typescript',
     'plugin:prettier/recommended',
-    'prettier/@typescript-eslint'
+    'prettier/@typescript-eslint',
   ],
-  rules: {
-  }
+  rules: {},
 };
