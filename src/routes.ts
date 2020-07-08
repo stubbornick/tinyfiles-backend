ec656cc7fad50571673f09de7377368b87e440fa
stubbornick <38818331+stubbornick@users.noreply.github.com>
2020-07-08 10:52:57 +0200

    Remove route prefix

--- src/routes.ts
+++ src/routes.ts
@@ -3,10 +3,6 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', async (req, res) => {
-  res.redirect('/api');
-});
-
-router.get('/api', async (req, res) => {
   res.send('It works!');
 });
 
