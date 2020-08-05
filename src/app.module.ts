502af97adff492596cfc7a69ae614f2e478673f6
stubbornick <38818331+stubbornick@users.noreply.github.com>
2020-08-05 20:48:35 +0200

    Delete App controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FileModule } from './file/file.module';
 
 @Module({
@@ -9,7 +7,5 @@ import { FileModule } from './file/file.module';
     TypeOrmModule.forRoot(),
     FileModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
